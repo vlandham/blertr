78c37ba9a0e4a398e566115db43c2a9e29ffbfbf
Jim V <jfv@stowers.org>
2011-06-29 13:20:05 -0500

    basic timing in place

--- lib/blertr/mail_notifier.rb
+++ lib/blertr/mail_notifier.rb
@@ -5,38 +5,38 @@ require 'net/smtp'
 module Blertr
   class MailNotifier < Notifier
     def initialize
+      super
       @name = "mail"
-      @config_file = File.join(File.dirname(__FILE__), "..", "..", "config", "mail_config.yaml")
     end
 
     def alert name, time
-      username = @options[:username]
-      password = @options[:password]
-      receiver = @options[:to]
-      msg = message name, time
-      smtp = Net::SMTP.new 'smtp.gmail.com', 587
-      smtp.enable_starttls
-      smtp.start('gmail.com', username, password, :login) do
-        smtp.send_message(msg, "#{username}@gmail.com", receiver)
+      begin
+        username = options[:username]
+        password = options[:password]
+        receiver = options[:to]
+        msg = message name, time
+        smtp = Net::SMTP.new 'smtp.gmail.com', 587
+        smtp.enable_starttls
+        smtp.start('gmail.com', username, password, :login) do
+          smtp.send_message(msg, "#{username}@gmail.com", receiver)
+        end
+      rescue
+        puts "error in #{name} alert"
       end
     end
 
     def message name, time
       first_name = name.split(" ")[0]
       msg = "Subject: #{first_name} is done!\n"
-      msg += "#{first_name} has completed.\nFull Command:#{name}\nTime Taken:#{time} seconds\n"
+      msg += "#{first_name} has completed.\nFull Command: #{name}\nTime Taken: #{time} seconds\n"
       msg
     end
 
     def can_alert?
-      rtn = File.exists? @config_file
-      if rtn
-        yaml_data = YAML::load(open(@config_file))
-        @options = Hash[yaml_data.map {|k,v| [k.to_sym, v]}] if yaml_data
-        rtn &= @options[:username]
-        rtn &= @options[:password]
-        rtn &= @options[:to]
-      end
+      rtn = true
+      rtn &= options[:username]
+      rtn &= options[:password]
+      rtn &= options[:to]
       rtn
     end
   end
