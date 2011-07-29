8be2eb8b420caf4a829876d9843165c54008f8ec
Jim V <jfv@stowers.org>
2011-07-29 11:21:45 -0500

    fill out email so it should send properly

--- lib/blertr/mail_notifier.rb
+++ lib/blertr/mail_notifier.rb
@@ -10,18 +10,24 @@ module Blertr
 
     def alert message
       username = options[:username]
+      blert_user = "#{username}@gmail.com"
       password = options[:password]
       receiver = options[:to]
-      msg = create_message(message)
+      msg = create_message(message, receiver, blert_user)
       smtp = Net::SMTP.new 'smtp.gmail.com', 587
       smtp.enable_starttls
-      smtp.start('gmail.com', username, password, :login) do
-        smtp.send_message(msg, "#{username}@gmail.com", receiver)
+      smtp.start('smtp.gmail.com', blert_user, password, :plain) do
+        smtp.send_message(msg, blert_user, receiver)
       end
     end
 
-    def create_message message
-      msg = "Subject: #{message.command_short_name} is done!\n"
+    def create_message message, to, from
+      date = Time.now.strftime("%a, %d %b %Y %H:%M:%S %z")
+      msg = "Date: #{date}\n"
+      msg += "From: Blertr App <#{from}>\n"
+      msg += "To: #{to} <#{to}>\n"
+      msg += "Subject: #{message.command_short_name} is done!\n"
+      msg += "\n"
       msg += "#{message.command_short_name} has completed.\nCommand: #{message.command}\nTime: #{message.time_string}\n"
       msg
     end
